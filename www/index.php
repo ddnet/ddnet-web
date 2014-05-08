2100181c3ce7c0ca17e21f5b286ac77628223380
def <dennis@felsin9.de>
2014-05-08 16:05:25 +0200

    upd

--- www/index.php
+++ www/index.php
@@ -44,7 +44,7 @@ function getOS() {
 }
 
 $user_os = getOS();
-$version = '3.8';
+$version = '3.8.1';
 
 if ($user_os == 'win') {
   print '<p class="download"><span class="big"><a href="/downloads/DDNet-' . $version . '-win32.zip">Download DDraceNetwork Client &amp; Server ' . $version . ' for Windows</a></span><br/><a href="/downloads/">Other systems and versions</a></p>';
