de9eb9b430bd5ba822004e27e085b9db09323673
def <dennis@felsin9.de>
2014-08-13 13:59:43 +0200

    upd

--- www/index.php
+++ www/index.php
@@ -48,7 +48,7 @@ function getOS() {
 }
 
 $user_os = getOS();
-$version = '4.7.2';
+$version = '4.7.4';
 
 if ($user_os == 'win') {
   print '<p class="download"><span class="big"><a href="/downloads/DDNet-' . $version . '-win32.zip">Download DDraceNetwork Client &amp; Server ' . $version . ' for Windows</a></span><br/><a href="/downloads/">Other systems and versions</a></p>';
@@ -69,6 +69,9 @@ if ($user_os == 'win') {
 <div class="block">
 <h2 id="news">News</h2>
 <ul>
+  <li>New security feature in DDNet Client 4.7.4 opens a new network connection, some Firewalls may ask if you want to do so</li>
+  <li>Hitomi server was removed. All maps were moved to Moderate and Brutal servers.</li>
+  <li>IP Spoofing protection for servers using <pre>sv_spoof_protection 1</pre></li>
   <li>DDNet Teamspeak server by laxa: <a href="ts3server://ts.ddnet.tw">ts.ddnet.tw</a></li>
   <li>How to use the new timeout protection:<br/>
     If you have DDNet client, after a timeout, you will automatically be reconnected to your tee when you rejoin. If it doesn't work, wait a bit more (100 seconds) and rejoin server.<br/>
@@ -81,7 +84,6 @@ if ($user_os == 'win') {
     <li>[Server] 1 minute punishment for saving to prevent abuse</li>
     <li>[Server] Fixes for /save and /load</li>
   </ul>
-    <img class="demo" src="neverwhere.png"/></li>
   <li><strong>DDNet 4.6.3</strong>:<br/>
   <ul>
     <li>[Client] Case insensitive player sort</li>
@@ -110,43 +112,6 @@ if ($user_os == 'win') {
     <li>[Server] Players join team 0 after finishing in team (without /lock)</li>
   </ul>
   <li>DDNet's planned successor <a href="http://hookrace.net/">HookRace</a> got a development blog</li>
-  <li><strong><a href="/tournament/22/">Quick Tournament #16: Moderate</a></strong> on Sunday, July 6 at 20:00 CEST (We had <a href="/over400.png">over 400</a> players on DDNet for the first time!)</li>
-  <li><strong>1 Year DDraceNetwork!</strong><br/>
-    DDraceNetwork will celebrate its first birthday on July 19 and 20 by holding a tournament with two maps. For the first time there will be prizes to be won. If you want to help, you can donate via PayPal:<br/>
-    <table>
-    <tr>
-    <td><strong>2 donations so far. Current prize money: 116.31€</strong></td>
-    <td><form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-      <input type="hidden" name="cmd" value="_s-xclick">
-      <input type="hidden" name="hosted_button_id" value="7WT2PXW6V2C3C">
-      <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-      <img alt="" border="0" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1">
-    </form></td>
-    </tr>
-    </table>
-    PayPal has high fees for donations, so if you want to donate without losing 4% to PayPal, send me a mail at <a href="mailto:deen@ddnet.tw">deen@ddnet.tw</a> and I'll give you my bank information. All donations will entirely go to the winners. Depending on how much prize money we get, we will decide how to split it and whether to host a third map! If you like some statistics, these were the first finishes on DDNet:
-    <table>
-    <tr><th>Date</th><th>Map</th><th>Player</th><th>Time</th></tr>
-    <tr><td>2013-07-18 03:38</td><td>Rainbow Sunrise</td><td>.me'</td><td>72:01</td></tr>
-    <tr><td>2013-07-18 03:38</td><td>Rainbow Sunrise</td><td>Rigmu</td><td>74:36</td></tr>
-    <tr><td>2013-07-18 17:05</td><td>Challenge</td><td>.me'</td><td>23:38</td></tr>
-    <tr><td>2013-07-18 17:05</td><td>Challenge</td><td>deen</td><td>23:32</td></tr>
-    <tr><td>2013-07-21 23:21</td><td>[SI] SELEN</td><td>Megaman | Cold</td><td>09:21</td></tr>
-    <tr><td>2013-07-21 23:21</td><td>[SI] SELEN</td><td>Hitomi</td><td>09:19</td></tr>
-    <tr><td>2013-07-21 23:35</td><td>[SI] KABOO</td><td>phacrum</td><td>12:18</td></tr>
-    <tr><td>2013-07-21 23:36</td><td>[SI] KABOO</td><td>Megaman | Cold</td><td>08:29</td></tr>
-    <tr><td>2013-07-21 23:36</td><td>[SI] KABOO</td><td>Hitomi</td><td>14:48</td></tr>
-    <tr><td>2013-07-21 23:36</td><td>[SI] KABOO</td><td>potsnacew</td><td>09:01</td></tr>
-    </table>
-    In the last 12 months DDNet got <strong>522 maps</strong>. By then <strong>154238 ranks</strong> were made, an average of <strong>295 ranks per map</strong> with an average time of <strong>19 minutes per map</strong> (excluding Flappy Bird). These ranks took a total of <strong>6 man-years</strong> (and that's without all the times where players don't finish!). About <strong>1200 people</strong> use DDNet client every day. At a good time <strong>350 players</strong> are online on DDNet at the same time. There are about <strong>2600 players on GER server</strong> every day, <strong>1300 in Chile</strong>, <strong>650 in South Africa</strong>, <strong>500 in Russia</strong>, <strong>350 in USA</strong> and <strong>150 in China</strong>. (DDNet and Blocker only)
-    <br/>
-    Thanks to everyone who helped build DDNet during this first year and all the players who found their ways on our servers. Special thanks to:<br/>
-    <ul>
-      <li>Lady Saavik for testing and fixing maps the entire time,</li>
-      <li>Broken for financing the USA server for 8 months now,</li>
-      <li>and all the testers and mappers, without which this would have been impossible.</li>
-    </ul></li>
-  <li><strong><a href="/tournament/21/">Quick Tournament #15: Brutal</a></strong> on Saturday, June 28 at 20:00 CEST</li>
   <li><strong>DDNet 4.4</strong>:<br/>
   <ul>
     <li>[Client] Feature: Auto-Reconnect (by CookieMichal)</li>
@@ -166,9 +131,7 @@ if ($user_os == 'win') {
     <li>[Server] Fix: Don't show draggers of players in solo part</li>
     <li>[Server] Fix: You can keep pistol when going through armor entities</li>
   </ul>
-  <li><strong><a href="/tournament/20/">Quick Tournament #14: Novice</a></strong> on Sunday, June 22 at 20:00 CEST</li>
   <li><a href="/downloads/DDNet-4.4.apk">Android version</a> of DDraceNetwork Client released (mainly for spectating and chatting; thanks to Pelya for porting Teeworlds to Android)</li>
-  <li><strong><a href="/tournament/19/">Brutal Tournament: Naufrage 2</a></strong> on Saturday, June 14 at 20:00 CEST</li>
   <li><strong>DDNet 4.2</strong>:<br/>
   <ul>
     <li>[Client] Fix: Don't crash when standing on start line and connecting dummy</li>
@@ -177,7 +140,6 @@ if ($user_os == 'win') {
     <li>[Editor] Fix: Show all images, no matter how many</li>
     <li>[Server] Feature: undeep, unsolo on test server</li>
   </ul>
-  <li><strong><a href="/tournament/18/">Quick Tournament #12: Moderate (1 star)</a></strong> on Saturday, June 7 at 20:00 CEST</li>
   <li><strong>DDNet 4.1</strong>:<br/>
   <ul>
     <li>Feature: News in client</li>
@@ -193,7 +155,6 @@ if ($user_os == 'win') {
   </ul>
   <li>New servers in South Africa thanks to <a href="/players/goo/">goo</a>, <a href="/players/lordtheuns/">lordtheuns</a>, <a href="/players/Lexy/">Lexy</a>! All DDNet locations now:<br/>
     <img class="demo" src="/locations.png" /></li>
-  <li><strong><a href="/tournament/17/">Quick Tournament #11: Brutal Solo Jetpack</a></strong> on Sunday, May 25 at 19:00 CEST</li>
   <li>How to hookfly with dummy: Hold the keys for hook and run and switch. Use hammerfly toggle key to make your dummy hook and stop hooking:<br/>
     <video class="fullvideo" controls>
       <source src="/client/dummyhookfly.mp4" type="video/mp4">
@@ -227,7 +188,6 @@ if ($user_os == 'win') {
     <li>Fix: Key presses more visible</li>
   </ul>
   <li><strong><a href="http://forum.ddnet.tw/viewtopic.php?f=3&t=25">Call for Testers</a></strong></li>
-  <li><strong><a href="/tournament/16/">Quick Tournament #10: Novice</a></strong> on Sunday, May 11 at 19:00 CEST</li>
   <li><a href="http://forum.ddnet.tw/">Official DDraceNetwork Forum!</a></li>
   <li><strong>DDNet 3.7</strong>:<br/>
   <ul>
