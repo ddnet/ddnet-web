780f3e8690e5d3fed0172f61481c0bd59b6ad675
def <dennis@felsin9.de>
2014-09-11 20:48:30 +0200

    upd

--- www/index.php
+++ www/index.php
@@ -20,9 +20,17 @@ menu: |
 <p>
 DDraceNetwork is a special version of DDRace, a <a href="https://www.teeworlds.com/">Teeworlds</a> modification. Help each other finish races with up to 64 players, compete against the best in <a href="/tournament/">international tournaments</a>, design your <a href="/howto/">own maps</a>, or run your <a href="/settingscommands/">own server</a>. The <a href="/status/">official servers</a> are located in Germany, USA, Russia, Iran, China, Chile and South Africa. All <a href="/ranks/">ranks</a> made on official servers are available everywhere and you can collect points!
 </p>
-<div class="startvideo"><div class="video-container"><iframe class="ytplayer" src="http://www.youtube.com/embed/4Y4YW9JzsT4?autoplay=0&hd=1"></iframe></div></div>
+<div class="startvideo"><div class="video-container"><iframe class="ytplayer" src="http://www.youtube.com/embed/wZfgs4wylRs?autoplay=0&hd=1"></iframe></div></div>
 <div class="startimages"><img class="demo" alt="Demo" src="lasers.png"/></div>
--<div class="startimages"><img class="demo" alt="Demo" src="full.png"/></div>
+<div class="startimages">
+  <p>Running DDNet servers costs money. If you want to help you can use this button to donate: (1.2% + 0.35€ PayPal fees)</p>
+  <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+  <input type="hidden" name="cmd" value="_s-xclick">
+  <input type="hidden" name="hosted_button_id" value="5NQ459VBKP4ZN">
+  <input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online.">
+  </form>
+  <p>In some countries you can <a href="https://www.paypal.com/myaccount/transfer/send">send money to friends for free using PayPal</a>. To help with paying the servers you can send money to deen@ddnet.tw.</p>
+</div>
 <!--<object id="live_embed_player_flash" width="800" height="450" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" bgcolor="#000000"><param name="allowFullScreen" value="true" /><param name="allowScriptAccess" value="always" /><param name="allowNetworking" value="all" /><param name="flashvars" value="hostname=de.twitch.tv&amp;channel=Hallowed1986&amp;auto_play=true&amp;start_volume=25" /><param name="src" value="http://de.twitch.tv/widgets/live_embed_player.swf?channel=Hallowed1986" /><embed id="live_embed_player_flash" width="800" height="450" type="application/x-shockwave-flash" src="http://de.twitch.tv/widgets/live_embed_player.swf?channel=Hallowed1986" allowFullScreen="true" allowScriptAccess="always" allowNetworking="all" flashvars="hostname=de.twitch.tv&amp;channel=Hallowed1986&amp;auto_play=true&amp;start_volume=25" bgcolor="#000000" /></object><iframe frameborder="0" scrolling="no" id="chat_embed" src="http://twitch.tv/chat/embed?channel=Hallowed1986&#038;popout_chat=true" height="450" width="300"></iframe>-->
 <br/>
 <div class="download"><img class="download-button" src="download.svg"/>
@@ -48,7 +56,7 @@ function getOS() {
 }
 
 $user_os = getOS();
-$version = '4.9.1';
+$version = '4.9.2';
 
 if ($user_os == 'win') {
   print '<p class="download"><span class="big"><a href="/downloads/DDNet-' . $version . '-win32.zip">Download DDraceNetwork Client &amp; Server ' . $version . ' for Windows</a></span><br/><a href="/downloads/">Other systems and versions</a></p>';
@@ -69,6 +77,16 @@ if ($user_os == 'win') {
 <div class="block">
 <h2 id="news">News</h2>
 <ul>
+  <li><a href="http://forum.ddnet.tw/viewtopic.php?f=3&t=407">New Moderate Tournament THIS Sunday at 20:00 CEST!</a></li>
+  <li><strong>DDNet 4.9.2</strong>:<br/>
+  <ul>
+    <li>[Client] Fix clamping in network objects to prevent many crashes</li>
+    <li>[Client] Fix: Reset zoom when going out of spectate mode (in zcatch for example)</li>
+    <li>[Client+Server] Threaded logger output for fewer lags</li>
+    <li>[Server] Ignore user inputs when paused</li>
+    <li>[Server] Instead of banning players for reconnecting too often, make them wait 3 seconds</li>
+  </ul></li>
+  <li><strong>DDoS protected DDNet FRA server running!</strong></li>
   <li><strong>DDNet 4.9</strong>:<br/>
   <ul>
     <li>[Client] Use settings_ddnet.cfg instead of settings.cfg</li>
