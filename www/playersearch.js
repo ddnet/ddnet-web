ff0929cf5a69747928e3d13715b1bf01fdc24424
def <dennis@felsin9.de>
2014-09-18 19:52:04 +0200

    upd

---
+++ www/playersearch.js
@@ -0,0 +1,21 @@
+var players = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  remote: '/players/?query=%QUERY',
+  limit: 10
+});
+
+players.initialize();
+
+$('#remote .typeahead').typeahead(null, {
+  name: 'players',
+  displayKey: 'name',
+  source: players.ttAdapter(),
+  templates: {
+    suggestion: function(o) {
+      return '<p><strong>' + o.name + '</strong><span class="right">' + o.points + '&nbsp;points</span></p>'
+    }
+  }
+}).on('typeahead:selected', function(e, data) {
+  $('#playerform').submit();
+});
