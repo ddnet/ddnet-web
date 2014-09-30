22d0458bcf88213dcf954b1b7dfe304cc73ad8e4
def <dennis@felsin9.de>
2014-09-30 20:01:36 +0200

    upd

--- www/index.php
+++ www/index.php
@@ -20,7 +20,7 @@ menu: |
 <p>
 DDraceNetwork is a special version of DDRace, a <a href="https://www.teeworlds.com/">Teeworlds</a> modification. Help each other finish races with up to 64 players, compete against the best in <a href="/tournament/">international tournaments</a>, design your <a href="/howto/">own maps</a>, or run your <a href="/settingscommands/">own server</a>. The <a href="/status/">official servers</a> are located in Germany, USA, Russia, Iran, China, Chile and South Africa. All <a href="/ranks/">ranks</a> made on official servers are available everywhere and you can collect points!
 </p>
-<div class="startvideo"><div class="video-container"><iframe class="ytplayer" src="http://www.youtube.com/embed/VLYfxtQwleM?autoplay=0&hd=1"></iframe></div><div align="right">More videos in the <a href="/halloffame/">Hall of Fame</a><br/>Tournament videos on <a href="https://www.youtube.com/channel/UCIoXLzJDSlF7FFDp1az8D4A">Hallowed1986's channel</a></div></div>
+<div class="startvideo"><div class="video-container"><iframe class="ytplayer" src="http://www.youtube.com/embed/r3jR-vLfnJs?autoplay=0&hd=1"></iframe></div><div align="right">More videos in the <a href="/halloffame/">Hall of Fame</a></div></div>
 <div class="startimages"><img class="demo" alt="Demo" src="lasers.png"/></div>
 <!--<div class="startvideo"><div class="video-container"><iframe src="http://hitbox.tv/#!/embed/Hallowed1986" frameborder="0" allowfullscreen></iframe></div></div>
 <div class="startimages"><iframe height="400" src="http://www.hitbox.tv/embedchat/Hallowed1986" frameborder="0" allowfullscreen></iframe></div>-->
@@ -58,7 +58,7 @@ function getOS() {
 }
 
 $user_os = getOS();
-$version = '5.0.2';
+$version = '5.1';
 
 if ($user_os == 'win') {
   print '<p class="download"><span class="big"><a href="/downloads/DDNet-' . $version . '-win32.zip">Download DDraceNetwork Client &amp; Server ' . $version . ' for Windows</a></span><br/><a href="/downloads/">Other systems and versions</a></p>';
@@ -79,6 +79,23 @@ if ($user_os == 'win') {
 <div class="block">
 <h2 id="news">News</h2>
 <ul>
+  <li><strong>DDNet 5.1</strong>:<br/>
+  <ul>
+    <li>[Client] Sort ingame menus (by laxa)</li>
+    <li>[Client] Print broadcasts in console (by laxa)</li>
+    <li>[Client] Add button to select whether to reset wanted weapon on death (by laxa)</li>
+    <li>[Editor] Fix editor input on Android</li>
+    <li>[Editor] Fix buggy envelope editor while tile picker is active</li>
+    <li>[Editor] Automapper support rotation (by DoNe)</li>
+    <li>[Editor] Disable key inputs in editor when typing in a field</li>
+    <li>[Editor] Fix: Selecting quads works after having rotated one</li>
+    <li>[Server] Kill protection: Prevent joining to spectators (by Savander)</li>
+    <li>[Server] Lock works for single player teams</li>
+    <li>[Server] Fix: Log player out of rcon after timeout protection was used</li>
+    <li>[Server] Check whether team has finished once a player in the team kills</li>
+    <li>[Server] Add sv_player_demo_record (mostly from Teerace, not used on official DDNet Servers)</li>
+    <li>[Server] Fix a bunch of crashes on server demo recording</li>
+  </ul></li>
   <li><a href="http://forum.ddnet.tw/viewtopic.php?f=3&t=453">Quick Tournament #20 with the Novice map "Kobra 3" by Zerodin, this Sunday at 20:00 CEST!</a> (German livestream will be on <a href="http://www.hitbox.tv/Hallowed1986">Hallowed1986's Hitbox channel</a>)<br/><img class="demo" src="http://forum.ddnet.tw/download/file.php?id=3216&mode=view" /></li>
   <li>DDNet Persian staying up thanks to KinG, bor, chucky &amp; F1rst</li>
   <li><a href="http://forum.ddnet.tw/viewtopic.php?f=3&t=426">Quick Tournament #19 with a Moderate (4 stars) map by Welf, this Sunday at 20:00 CEST!</a> (German livestream will be on <a href="http://www.hitbox.tv/Hallowed1986">Hallowed1986's Hitbox channel</a>)</li>
