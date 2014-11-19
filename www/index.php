3a1ed27b74c7464d3caf87b0c36f5f312c6d5c6f
def <dennis@felsin9.de>
2014-11-19 21:52:32 +0100

    upd

--- www/index.php
+++ www/index.php
@@ -20,7 +20,7 @@ menu: |
 <p>
 DDraceNetwork is a special version of DDRace, a <a href="https://www.teeworlds.com/">Teeworlds</a> modification. Help each other finish races with up to 64 players, compete against the best in <a href="/tournaments/">international tournaments</a>, design your <a href="/howto/">own maps</a>, or run your <a href="/settingscommands/">own server</a>. The <a href="/status/">official servers</a> are located in Germany, USA, Russia, Iran, China, Chile and South Africa. All <a href="/ranks/">ranks</a> made on official servers are available everywhere and you can collect points!
 </p>
-<div class="startvideo"><div class="video-container"><iframe class="ytplayer" src="http://www.youtube.com/embed/k4RCUCJAnEE?autoplay=0&hd=1"></iframe></div><div align="right">More videos in the <a href="/halloffame/">Hall of Fame</a></div></div>
+<div class="startvideo"><div class="video-container"><iframe class="ytplayer" src="http://www.youtube.com/embed/I8T3EFRgEkg?autoplay=0&hd=1"></iframe></div><div align="right">More videos in the <a href="/halloffame/">Hall of Fame</a></div></div>
 <div class="startimages"><img class="demo" alt="Demo" src="lasers.png"/></div>
 <!--<div class="startvideo"><div class="video-container"><iframe src="http://hitbox.tv/#!/embed/Hallowed1986" frameborder="0" allowfullscreen></iframe></div></div>
 <div class="startimages"><iframe height="400" src="http://www.hitbox.tv/embedchat/Hallowed1986" frameborder="0" allowfullscreen></iframe></div>-->
@@ -79,7 +79,6 @@ if ($user_os == 'win') {
     <li>[Client] gfx_text_overlay to stop rendering text at far distances</li>
     <li>[Server] Improve performance and ping</li>
     <li>[Server] Improve /rank performance</li>
-    <li>[Server] Fix possible server crash</li>
   </ul></li>
   <li>DDNet CAN now running in Canada thanks to o_be_one from r0x.fr! All locations now:<br/>
     <img class="demo" src="/locations.png" /></li>
