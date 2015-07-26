6bed9eb371ed295cf1f4e3539cf9bcbe1c7aa5a5
def <dennis@felsin9.de>
2015-07-26 15:41:46 +0200

    upd

--- www/index.php
+++ www/index.php
@@ -59,7 +59,7 @@ function getOS() {
 }
 
 $user_os = getOS();
-$version = '7.9.1';
+$version = '8.0';
 
 if ($user_os == 'win32') {
   print '<p class="download"><span class="big"><a href="/downloads/DDNet-' . $version . '-win32.zip">Download DDraceNetwork Client &amp; Server ' . $version . ' for Windows (32bit)</a></span><br/><a href="/downloads/">Other systems and versions</a> | <a href="http://teewebs.net/">Try it out in your browser</a></p>';
@@ -91,6 +91,24 @@ if ($user_os == 'win32') {
 <div class="block">
 <h2 id="news">News</h2>
 <ul>
+  <li><strong>DDNet 8.0</strong>:<br/>
+  <ul>
+    <li>[Client] Allow spectating other players and freeview in demo player (especially fun with /showall and /showothers on)</li>
+    <li>[Client] Add foes feature to permanantly ignore players (F1, add_foe only)</li>
+    <li>[Client] Use temporary file for saving config to prevent broken configs</li>
+    <li>[Client] Fix: Make add_demomarker working in all recorders (by east)</li>
+    <li>[Client] Fix highlighting in demo playback (by east)</li>
+    <li>[Client] Fix: TAB in console doesn't influence server browser</li>
+    <li>[Client] Fix scroll bar in server control votes when search bar is used</li>
+    <li>[Server] Add switch_open to set the initial state of a switch to open</li>
+    <li>[Server] Fix support for 0 stars in database</li>
+    <li>[Server] Fix /mapinfo with mixed case server names (DDmaX)</li>
+    <li>[Editor] Fix envelope button color (by DoNe)</li>
+    <li>[Editor] Save twice as fast (hopefully doesn't break anything)</li>
+    <li>[Editor] Add "Save copy" button so that filename is not changed</li>
+    <li>[Editor] Add ctrl+shift+s for "save as" and ctrl+shift+alt+s for "save copy"</li>
+    <li>[Editor] Zoom faster by holding down numpad +/- (by Chairn)</li>
+  </ul</li>
   <li><a href="http://forum.ddnet.tw/viewtopic.php?f=3&p=19036">The History of DDNet</a></li>
   <li><strong>DDNet 7.9.1</strong>:<br/>
   <ul>
@@ -118,39 +136,5 @@ if ($user_os == 'win32') {
   <li>DDNet South Africa on new host!</br>
 After we had lots of connection problems on our last host, we now have a new server in South Africa, thanks to lordtheuns!</li>
   <li><a href="/funding/">DDNet Funding</a></li>
-  <li><strong>DDNet 7.8.2</strong>:<br/>
-  <ul>
-    <li>[Client] 64bit Windows version available</li>
-    <li>[Client] Fix: Error out on invalid UTF-8 strings (by heinrich5991)</li>
-    <li>[Client] Add size variable for clan plates</li>
-    <li>[Client] Fix: Make TAB always work in serverbrowser</li>
-    <li>[Client] More reasonable default key bindings</li>
-    <li>[Editor] Highlight game layers</li>
-    <li>[Editor] Make a few value selectors wrap around</li>
-    <li>[Editor] Display local time in editor so mappers don't lose track of it</li>
-    <li>[Server] Add sv_rescue_delay and rename sv_allow_rescue to sv_rescue</li>
-    <li>[Server] Update unicode confusable detection</li>
-  </ul></li>
-  <li><strong>DDNet 7.8</strong>:<br/>
-  <ul>
-    <li>[Client] Add automatic antiping based on ping</li>
-    <li>[Client] Add support to show clan above name plates</li>
-    <li>[Client] Improve Android client</li>
-    <li>[Client] Show full time in scoreboard even when minutes are over 100</li>
-    <li>[Client] Fix autorecorder again (by east)</li>
-    <li>[Client] Better Polish translations (by Savander and Lady Saavik)</li>
-    <li>[Client] Add Catalan translations (by Ryozuki)</li>
-    <li>[Editor] Ctrl-s doesn't ask about overwriting anymore</li>
-    <li>[Mapping] Improved grass_main and round-tiles automapper rules (by hi_leute_gll)</li>
-    <li>[Server] If no rcon password is set, generate one (by heinrich5991)</li>
-    <li>[Server] /mapinfo shows release dates now</li>
-    <li>[Server] Fix excessive name/skin changing</li>
-    <li>[Server] Hopefully fix problem where timeout code sometimes doesn't work</li>
-  </ul></li>
-  <li>Useful binds to show more of chat when you start chatting:<br/>
-    <pre>bind t "+show_chat; chat all"
-bind y "+show_chat; chat team"
-bind i "+show_chat; chat all /c "</pre></li>
-  <li><a href="http://forum.ddnet.tw/viewtopic.php?f=6&t=1637">DDNet Code of Conduct</a></li>
 </ul>
 </div>
