1278a723918eada57d007751f250f4d2c3384cbb
def <dennis@felsin9.de>
2015-08-27 22:59:51 +0200

    upd

--- phpbb/file.php
+++ phpbb/file.php
@@ -238,7 +238,7 @@ else
 	}
 
 	if ($post_row['forum_id'] == 9 && ($attachment['extension'] == 'map' || $attachment['extension'] == 'cfg') &&
-		($user->data['group_id'] != 5 && $user->data['group_id'] != 4 && $user->data['group_id'] != 8 && $user->data['group_id'] != 10 && $user->data['group_id'] != 13))
+		($user->data['group_id'] != 5 && $user->data['group_id'] != 4 && $user->data['group_id'] != 8 && $user->data['group_id'] != 10 && $user->data['group_id'] != 13 && $user->data['group_id'] != 17))
 	{
 		$sql = 'SELECT topic_poster
 			FROM ' . TOPICS_TABLE . "
