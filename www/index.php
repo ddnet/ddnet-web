5fc7a2b9162df270d723310c9fbc853368a7d127
H-M-H <henry@freedesk.net>
2016-01-05 19:41:03 +0100

    upd

--- www/index.php
+++ www/index.php
@@ -84,7 +84,7 @@ function getOS() {
 }
 
 $user_os = getOS();
-$version = '9.0.2';
+$version = '9.1';
 
 if ($user_os == 'win32') {
   print '<p class="download"><span class="big"><a href="/downloads/DDNet-' . $version . '-win32.zip">Download DDraceNetwork Client &amp; Server ' . $version . ' for Windows (32bit)</a></span><br/><a href="/downloads/">Other systems and versions</a> | <a href="http://teewebs.net/">Try it out in your browser</a></p>';
