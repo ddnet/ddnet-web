bd6a50a48c60970934033de4cb915e5200fb8865
H-M-H <henry@freedesk.net>
2016-01-16 22:55:07 +0100

    added DDNet-YouTube channel to frontpage

--- www/index.php
+++ www/index.php
@@ -105,6 +105,7 @@ if ($user_os == 'win32') {
 ?>
 <a href="feed/"><img src="feed.svg" alt="Feed"/></a>
 <a href="https://facebook.com/DDraceNetwork"><img src="facebook.svg" alt="Facebook"/></a>
+<a href="https://www.youtube.com/user/DDraceNetwork"><img src="YouTube.svg" alt="YouTube"/></a>
 <a href="https://github.com/ddnet/"><img src="github.svg" alt="GitHub"/></a>
 
 <div class="right">
