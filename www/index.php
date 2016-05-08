ce6572a176a2e70b7b156a69d62aa4b027981ecf
H-M-H <henry@freedesk.net>
2016-05-08 22:19:26 +0200

    new video

--- www/index.php
+++ www/index.php
@@ -33,7 +33,7 @@ $(document).ready(function() { $("#jsclient").focus() });
 </script>
 -->
 <div class="startvideo"><div class="video-container">
-	<iframe allowfullscreen class="ytplayer" src="https://www.youtube.com/embed/bilmnke_Lcs?autoplay=0&hd=1&iv_load_policy=3"></iframe>
+	<iframe allowfullscreen class="ytplayer" src="https://www.youtube.com/embed/veB2xSMsizw?autoplay=0&hd=1&iv_load_policy=3"></iframe>
 </div></div>
 <div class="startimages">
 	<a href="www.ddnet.tw.png"><img class="demo" alt="Demo" src="www.ddnet.tw.png"/></a>
