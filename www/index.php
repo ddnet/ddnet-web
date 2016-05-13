50b26c06c884f9e86618e0830205536678386e8a
def <dennis@felsin9.de>
2016-05-13 20:07:13 +0200

    Remove funding info

--- www/index.php
+++ www/index.php
@@ -2,7 +2,6 @@
 layout: default
 title: DDraceNetwork
 head: |
-  <link rel="stylesheet" href="funding/jquery-ui-1.8.22.custom.css" />
   <link rel="alternate" type="application/atom+xml" title="DDNet News" href="/feed/">
 menu: |
   <ul>
@@ -107,14 +106,6 @@ if ($user_os == 'win32') {
 <a href="https://www.youtube.com/user/DDraceNetwork"><img src="youtube.svg" alt="YouTube"/></a>
 <a href="feed/"><img src="feed.svg" alt="Feed"/></a>
 <a href="https://github.com/ddnet/"><img src="github.svg" alt="GitHub"/></a>
-
-<div class="right">
-  <a href="/funding/"><div class="progressbar" id="funding-total" style="width: 20em;"><div class="progress-label"></div></div></a>
-  <a href="/funding/"><div class="progressbar" id="funding-old" style="width: 20em;"><div class="progress-label"></div></div></a>
-  <script src="/jquery.js" type="text/javascript"></script>
-  <script src="funding/jquery-ui-1.8.22.custom.min.js" type="text/javascript"></script>
-  {% include funding.html %}
-</div>
 </div>
 <br/>
 </div>
