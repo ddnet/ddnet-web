1a40f079cb8d857b0c3e56a5a63c9a95d26b6da0
user578 <d8v.info@gmail.com>
2016-05-31 01:18:00 +0300

    Fix jQuery selectors in columnize function

--- www/players-data/sorter.js
+++ www/players-data/sorter.js
@@ -40,8 +40,8 @@ $(function(){
         table3 = $(table2).next();
 
         // return if it's already columnized
-        if ($(table2 + ':has(tr)') &&
-            $(table3 + ':has(tr)'))
+        if ($(table2).find('tbody >tr').length ||
+            $(table3).find('tbody >tr').length)
             return;
 
         // set minimum table row height
