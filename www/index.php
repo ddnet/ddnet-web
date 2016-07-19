6b39516ff9422d170891aad3d2127808be244f0a
Ryan <ryan.goniwiecha@gmail.com>
2016-07-19 13:02:05 -0400

    Changed Video

--- www/index.php
+++ www/index.php
@@ -32,7 +32,7 @@ $(document).ready(function() { $("#jsclient").focus() });
 </script>
 -->
 <div class="startvideo"><div class="video-container">
-	<iframe allowfullscreen class="ytplayer" src="https://www.youtube.com/embed/Rr5y7ErLtmU?autoplay=0&hd=1&iv_load_policy=3"></iframe>
+	<iframe allowfullscreen class="ytplayer" src="https://www.youtube.com/embed/EC-vCSBr0mM?autoplay=0&hd=1&iv_load_policy=3"></iframe>
 </div></div>
 <div class="startimages">
 	<a href="www.ddnet.tw.png"><img class="demo" alt="Demo" src="www.ddnet.tw.png"/></a>
