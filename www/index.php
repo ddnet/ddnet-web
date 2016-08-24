24eafd6fc193961225ce699892e12b3f8bbb8edf
Dennis Felsing <dennis@felsin9.de>
2016-08-24 11:20:03 +0200

    DDNet 10.3

--- www/index.php
+++ www/index.php
@@ -78,7 +78,7 @@ function getOS() {
 }
 
 $user_os = getOS();
-$version = '10.2.1';
+$version = '10.3';
 
 if ($user_os == 'win32') {
   print '<p class="download"><span class="big"><a href="/downloads/DDNet-' . $version . '-win32.zip">Download DDraceNetwork Client &amp; Server ' . $version . ' for Windows (32bit)</a></span><br/><a href="/downloads/">Other systems and versions</a></p>';
