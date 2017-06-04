fecf9a3233750201e1b10a217615b8de7302aacc
def <dennis@felsin9.de>
2017-06-04 23:32:17 +0200

    More lightweight youtube embed on frontpage (can we do this on forum too?)

--- www/index.php
+++ www/index.php
@@ -22,34 +22,23 @@ menu: |
 <p>
 DDraceNetwork (DDNet) is an actively maintained version of DDRace, a <a href="https://www.teeworlds.com/">Teeworlds</a> modification with a unique cooperative gameplay. Help each other play through <a href="/releases/">custom maps</a> with up to 64 players, compete against the best in <a href="/tournaments/">international tournaments</a>, design your <a href="/howto/">own maps</a>, or run your <a href="/settingscommands/">own server</a>. The <a href="/status/">official servers</a> are located in Germany, Russia, USA, Canada, China, Chile, Brazil, South Africa and Australia. All <a href="/ranks/">ranks</a> made on official servers are available worldwide and you can <a href="/players/milk">collect points</a>!
 </p>
-<!--
-<div class="video-container"><iframe id="jsclient" class="ytplayer" src="http://teewebs.net/"></iframe></div>
-<div align="right"><a href="http://teewebs.net/">DDnet JS client by eeeee</a></div>
-<script src="/jquery.js" type="text/javascript"></script>
-<script type="text/javascript">
-$(document).click(function() { $("#jsclient").focus() });
-$(document).ready(function() { $("#jsclient").focus() });
-</script>
--->
 <div class="startvideo"><div class="video-container">
-	<iframe allowfullscreen class="ytplayer" src="https://www.youtube.com/embed/jTP-Oz05IN4?autoplay=0&hd=1&iv_load_policy=3"></iframe>
+  <div class="ytplayer" data-id="jTP-Oz05IN4"></div>
 </div></div>
 <div class="startimages">
 	<a href="www.ddnet.tw.png"><img class="demo" alt="Demo" src="www.ddnet.tw.png"/></a>
 	<a href="locations.png"><img class="demo" alt="Demo" src="locations.png"/></a>
 </div>
-<!--
+{% comment %}
 <div class="startvideo">
 	<div class="video-container"><iframe src="http://hitbox.tv/#!/embed/vasten100" frameborder="0" allowfullscreen></iframe></div>
 </div>
 <div class="startimages">
 	<iframe height="400" src="http://www.hitbox.tv/embedchat/vasten100" frameborder="0" allowfullscreen></iframe>
 </div>
--->
-<!--
 <iframe id="live_embed_player_flash" src="https://player.twitch.tv/?channel=hallowed1986" frameborder="0" scrolling="no" height="378" width="620"></iframe><a href="https://www.twitch.tv/hallowed1986?tt_medium=live_embed&tt_content=text_link" style="padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px;text-decoration:underline;">Watch live video from Hallowed1986 on www.twitch.tv</a>
 <iframe id="chat_embed" src="https://www.twitch.tv/hallowed1986/chat?popout=" frameborder="0" scrolling="no" height="500" width="350"></iframe>
--->
+{% endcomment %}
 <br/>
 <div class="download"><img class="download-button" src="download.svg" alt="Download"/>
 <?php
@@ -122,3 +111,56 @@ if ($user_os == 'win32') {
   </div>
 {% endfor %}
 </div>
+
+<script>
+    var div, n,
+        v = document.getElementsByClassName("ytplayer");
+    for (n = 0; n < v.length; n++) {
+        div = document.createElement("div");
+        div.setAttribute("data-id", v[n].dataset.id);
+        div.innerHTML = '<img src="https://i.ytimg.com/vi/' + v[n].dataset.id + '/hqdefault.jpg"><div class="play"></div>';
+        div.onclick = ytiframe;
+        v[n].appendChild(div);
+    }
+
+    function ytiframe() {
+        var iframe = document.createElement("iframe");
+        iframe.setAttribute("src", 'https://www.youtube.com/embed/' + this.dataset.id + '?autoplay=1');
+        iframe.setAttribute("frameborder", "0");
+        iframe.setAttribute("allowfullscreen", "1");
+        this.parentNode.replaceChild(iframe, this);
+    }
+</script>
+<style>
+    .ytplayer img {
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        display: block;
+        margin: auto;
+        width: 100%;
+        position: absolute;
+        height: auto;
+        cursor: pointer;
+        -webkit-transition: .4s all;
+        -moz-transition: .4s all;
+        transition: .4s all;
+    }
+
+    .ytplayer img:hover {
+        -webkit-filter: brightness(75%);
+    }
+
+    .ytplayer .play {
+        height: 72px;
+        width: 72px;
+        left: 50%;
+        top: 50%;
+        margin-left: -36px;
+        margin-top: -36px;
+        position: absolute;
+        background: url("/youtube-play.png") no-repeat;
+        pointer-events: none;
+    }
+</style>
