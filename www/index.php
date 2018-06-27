098c2c4515f8e7486381074c758ef27f82494ad5
Dennis Felsing <dennis@felsin9.de>
2018-06-27 09:39:35 +0200

    11.2

--- www/index.php
+++ www/index.php
@@ -98,7 +98,7 @@ function getOS() {
 }
 
 $user_os = getOS();
-$version = '11.1.9';
+$version = '11.2';
 
 if ($user_os == 'win32') {
   print '<p class="download"><span class="big"><a href="/downloads/DDNet-' . $version . '-win32.zip">Download DDraceNetwork Client &amp; Server ' . $version . ' for Windows (32bit)</a></span><br/><a href="/downloads/">Other systems and versions</a></p>';
