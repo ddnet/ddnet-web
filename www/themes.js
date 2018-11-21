b18c517fb8d3e3860c7345e7f1875e33676f3fab
xse <elouan.pignet@gmail.com>
2018-11-21 12:47:02 +0000

    bad autoindent

--- www/themes.js
+++ www/themes.js
@@ -1,43 +1,41 @@
-    var available_theme = "/css-halloween.css";
-    const default_theme = "";
-    var user_theme = "";
+var available_theme = "/css-halloween.css";
+const default_theme = "";
+var user_theme = "";
 
-    function onthemeselect() {
-      if(document.getElementById("theme_checkbox").checked) {
-        var styles = available_theme;
-        var newSS=document.createElement('link');
-        newSS.rel='stylesheet';
-        newSS.id='user_theme';
-        newSS.href=styles;
-        document.getElementsByTagName("head")[0].appendChild(newSS);
-        localStorage.setItem("user_theme", available_theme);
-      } else {
-        var element = document.getElementById('user_theme');
-        element.parentNode.removeChild(element);
-        localStorage.setItem("user_theme", default_theme);
-      }
-      
+function onthemeselect() {
+  if(document.getElementById("theme_checkbox").checked) {
+    var styles = available_theme;
+    var newSS=document.createElement('link');
+    newSS.rel='stylesheet';
+    newSS.id='user_theme';
+    newSS.href=styles;
+    document.getElementsByTagName("head")[0].appendChild(newSS);
+    localStorage.setItem("user_theme", available_theme);
+  } else {
+    var element = document.getElementById('user_theme');
+    element.parentNode.removeChild(element);
+    localStorage.setItem("user_theme", default_theme);
+  }
+
+}
+document.addEventListener("DOMContentLoaded", function(event) {
+  if(localStorage.user_theme != null) {
+    console.log("localStorage.user_theme found !");
+    user_theme = localStorage.getItem('user_theme');
+    console.log('user_theme is: '+user_theme);
+    if(user_theme != "") {
+      document.getElementById("theme_checkbox").checked = true;
+      onthemeselect();
     }
-    document.addEventListener("DOMContentLoaded", function(event) {
-      if(localStorage.user_theme != null) {
-        console.log("localStorage.user_theme found !");
-        user_theme = localStorage.getItem('user_theme');
-        console.log('user_theme is: '+user_theme);
-        if(user_theme != "") {
-          document.getElementById("theme_checkbox").checked = true;
-          onthemeselect();
-        }
-        // case when stored theme is not default nor available theme, like if available theme gets changed
-        if(user_theme != "" && user_theme != available_theme) {
-        user_theme = default_theme;
-        localStorage.setItem('user_theme',user_theme);
-        }
-      } else {
-        console.log("Storing default theme");
-        //user_theme = default_theme;
-        localStorage.setItem("user_theme", user_theme);
-        document.getElementById("theme_checkbox").checked = false;
-        //onthemeselect();
+    // case when stored theme is not default nor available theme, like if available theme gets changed
+    if(user_theme != "" && user_theme != available_theme) {
+      user_theme = default_theme;
+      localStorage.setItem('user_theme',user_theme);
+    }
+    } else {
+      console.log("Storing default theme");
+      localStorage.setItem("user_theme", user_theme);
+      document.getElementById("theme_checkbox").checked = false;
       }
       document.getElementById("theme_checkbox").addEventListener("change", onthemeselect);
-    });
+});
