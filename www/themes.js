fcc3b56b6a4898eb9217eaf411470984c8a73e40
xse <elouan.pignet@gmail.com>
2018-11-21 13:18:31 +0000

    handle default theme + cleanup

--- www/themes.js
+++ www/themes.js
@@ -4,6 +4,17 @@ var user_theme = "";
 
 function onthemeselect() {
   if(document.getElementById("theme_checkbox").checked) {
+    settheme();
+  } else {
+    var element = document.getElementById('user_theme');
+    if(element) {
+      element.parentNode.removeChild(element);
+    }
+    localStorage.setItem("user_theme", "");
+  }
+}
+
+function settheme() {
     var styles = available_theme;
     var newSS=document.createElement('link');
     newSS.rel='stylesheet';
@@ -11,31 +22,25 @@ function onthemeselect() {
     newSS.href=styles;
     document.getElementsByTagName("head")[0].appendChild(newSS);
     localStorage.setItem("user_theme", available_theme);
-  } else {
-    var element = document.getElementById('user_theme');
-    element.parentNode.removeChild(element);
-    localStorage.setItem("user_theme", default_theme);
-  }
-
 }
+
 document.addEventListener("DOMContentLoaded", function(event) {
   if(localStorage.user_theme != null) {
-    console.log("localStorage.user_theme found !");
     user_theme = localStorage.getItem('user_theme');
-    console.log('user_theme is: '+user_theme);
     if(user_theme != "") {
       document.getElementById("theme_checkbox").checked = true;
       onthemeselect();
     }
-    // case when stored theme is not default nor available theme, like if available theme gets changed
     if(user_theme != "" && user_theme != available_theme) {
       user_theme = default_theme;
       localStorage.setItem('user_theme',user_theme);
     }
     } else {
-      console.log("Storing default theme");
-      localStorage.setItem("user_theme", user_theme);
-      document.getElementById("theme_checkbox").checked = false;
+      if(default_theme != "") {
+        document.getElementById("theme_checkbox").checked = true;
       }
-      document.getElementById("theme_checkbox").addEventListener("change", onthemeselect);
+      localStorage.setItem("user_theme", default_theme);
+      onthemeselect();
+    }
+    document.getElementById("theme_checkbox").addEventListener("change", onthemeselect);
 });
