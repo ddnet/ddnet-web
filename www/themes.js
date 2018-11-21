83b029825545535dc5e9ce516e4ba0966d14d829
xse <elouan.pignet@gmail.com>
2018-11-21 19:50:52 +0000

    subdomain cookie storage & stuff

--- www/themes.js
+++ www/themes.js
@@ -6,11 +6,10 @@ function onthemeselect() {
   if(document.getElementById("theme_checkbox").checked) {
     settheme();
   } else {
-    var element = document.getElementById('user_theme');
-    if(element) {
-      element.parentNode.removeChild(element);
+    if(document.getElementById('user_theme')) {
+      document.getElementById('user_theme').parentNode.removeChild(document.getElementById('user_theme'));
     }
-    localStorage.setItem("user_theme", "");
+    setCookie("user_theme", "");
   }
 }
 
@@ -21,26 +20,50 @@ function settheme() {
   newSS.id='user_theme';
   newSS.href=styles;
   document.getElementsByTagName("head")[0].appendChild(newSS);
-  localStorage.setItem("user_theme", available_theme);
+  setCookie("user_theme", available_theme);
+}
+
+function setCookie(key,value) {
+  var d = new Date();
+  d.setFullYear(2345);
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = key + "=" + value + ";" + expires + ";path=/"+";domain=ddnet.tw";
+}
+
+function getCookie(key) {
+  var name = key + "="; // key += "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "nocookie";
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  if(localStorage.user_theme != null) {
-    user_theme = localStorage.getItem('user_theme');
+  var user_theme=getCookie("user_theme");
+  if(user_theme != "") {
+    settheme();
+    if(document.getElementById('theme_checkbox')) {
+      document.getElementById("theme_checkbox").checked = true;
+    }
+  } else if((user_theme != "" && user_theme != available_theme) || user_theme == "nocookie") {
+    user_theme = default_theme;
+    setCookie("user_theme", user_theme);
     if(user_theme != "") {
-      document.getElementById("theme_checkbox").checked = true;
-      onthemeselect();
+      settheme();
+      if(document.getElementById('theme_checkbox')) {
+        document.getElementById("theme_checkbox").checked = true;
+      }
     }
-    if(user_theme != "" && user_theme != available_theme) {
-      user_theme = default_theme;
-      localStorage.setItem('user_theme',user_theme);
-    }
-  } else {
-    if(default_theme != "") {
-      document.getElementById("theme_checkbox").checked = true;
-    }
-    localStorage.setItem("user_theme", default_theme);
-    onthemeselect();
   }
-  document.getElementById("theme_checkbox").addEventListener("change", onthemeselect);
+  if(document.getElementById('theme_checkbox')) {
+    document.getElementById("theme_checkbox").addEventListener("change", onthemeselect);
+  }
 });
