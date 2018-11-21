fd2c3ace25881f5932c1fba8da85c4fbce5f7701
xse <elouan.pignet@gmail.com>
2018-11-21 12:11:44 +0000

    use checkbox + less/simpler code + basic checkbox style

--- www/themes.js
+++ www/themes.js
@@ -1,53 +1,41 @@
+    var available_theme = "/css-halloween.css";
+    const default_theme = "";
+    var user_theme = "";
 
-// css in the order they appears in default/post.html
-// 2 would be another theme made like halloween (by tweaking values and not rewriting a complete css)
-const user_themes_default = {
-  0: true, // css.css
-  1: false // css-halloween.css
-};
-var user_themes = {};
-
-// enable/disable css from data in user_themes object
-// saves modified user_themes to localStorage
-function settheme(bar) {
-  for (const [id, activated] of Object.entries(bar)) {
-    if(activated == false) {
-      document.styleSheets[id].disabled = true;
+    function onthemeselect() {
+      if(document.getElementById("theme_checkbox").checked) {
+        var styles = available_theme;
+        var newSS=document.createElement('link');
+        newSS.rel='stylesheet';
+        newSS.id='user_theme';
+        newSS.href=styles;
+        document.getElementsByTagName("head")[0].appendChild(newSS);
+        localStorage.setItem('user_theme',available_theme);
+      } else {
+        var element = document.getElementById('user_theme');
+        element.parentNode.removeChild(element);
+        localStorage.setItem('user_theme',default_theme);
+      }
+      
     }
-    if(activated == true) {
-      document.styleSheets[id].disabled = false;
-      document.getElementById("themeselect").selectedIndex = id; // displays activated theme in the select thingy
-    }
-  }
-  localStorage.setItem('user_themes', JSON.stringify(bar));
-}
-
-// triggers when a new theme is selected
-// sort values in user_themes and send that to settheme
-function onthemeselect() {
-  var foo = document.getElementById("themeselect");
-  var selected = foo.options[foo.selectedIndex].value;
-  for (const [id, activated] of Object.entries(user_themes)) {
-    if(id == 0) {
-      user_themes[id] = true; // so far css.css should always be displayed
-    } else if(id == selected) {
-      user_themes[id] = true;
-    } else {
-      user_themes[id] = false;
-    }
-  }
-  settheme(user_themes);
-}
-
-// check if user_theme exists, if so set the theme right
-// if not store the default theme setting
-if(localStorage.user_themes) {
-  user_themes = JSON.parse(localStorage.getItem('user_themes'));
-  settheme(user_themes);
-} else {
-  user_themes = user_themes_default;
-  localStorage.setItem('user_themes', JSON.stringify(user_themes));
-  settheme(user_themes);
-}
-
-document.getElementById("themeselect").addEventListener("change", onthemeselect);
+    document.addEventListener("DOMContentLoaded", function(event) {
+      if(localStorage.user_theme != null) {
+        console.log("localStorage.user_theme found !");
+        user_theme = localStorage.getItem('user_theme');
+        console.log('user_theme is: '+user_theme);
+        if(user_theme != "") {
+          document.getElementById("theme_checkbox").checked = true;
+          onthemeselect();
+        }
+        // case when stored theme is not default nor available theme, like if available theme gets changed
+        if(user_theme != "" && user_theme != available_theme) {
+        user_theme = default_theme;
+        localStorage.setItem('user_theme',user_theme);
+        }
+      } else {
+        console.log("Storing default theme");
+        user_theme = default_theme;
+        localStorage.setItem('user_theme',user_theme);
+      }
+      document.getElementById("theme_checkbox").addEventListener("change", onthemeselect);
+    });
