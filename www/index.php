970d70be52cee6201e46ef44dfcd98769f50e648
def <dennis@felsin9.de>
2019-03-28 23:51:20 +0100

    Version 12.0

--- www/index.php
+++ www/index.php
@@ -99,7 +99,7 @@ function getOS() {
 }
 
 $user_os = getOS();
-$version = '11.9';
+$version = '12.0';
 
 if ($user_os == 'win32') {
   print '<p class="download"><span class="big"><a href="/downloads/DDNet-' . $version . '-win32.zip">Download DDraceNetwork Client &amp; Server ' . $version . ' for Windows (32bit)</a></span><br/><a href="/downloads/">Other systems and versions</a></p>';
