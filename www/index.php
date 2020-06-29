a7d1594a00d101fc43a51faef7f0b187efa15fce
def <dennis@felsin9.de>
2020-06-29 10:02:58 +0200

    Revert "Version 14.0.3"
    This reverts commit 5bc48516300d44a1cd00530c9cab367b4b7d5309.

--- www/index.php
+++ www/index.php
@@ -99,7 +99,7 @@ function getOS() {
 }
 
 $user_os = getOS();
-$version = '14.0.3';
+$version = '14.0.2';
 
 if ($user_os == 'win32') {
   print '<p class="download"><span class="big"><a href="/downloads/DDNet-' . $version . '-win32.zip">Download DDraceNetwork Client &amp; Server ' . $version . ' for Windows (32bit)</a></span><br/><a href="/downloads/">Other systems and versions, changelogs</a></p>';
