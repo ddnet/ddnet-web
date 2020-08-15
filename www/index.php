6b3d7791ae50649d70b34488f66c74bc37eae57e
def <dennis@felsin9.de>
2020-08-15 21:40:34 +0200

    LCSG47 stream

--- www/index.php
+++ www/index.php
@@ -49,7 +49,7 @@ DDraceNetwork (DDNet) is an actively maintained version of DDRace, a <a href="ht
   new Twitch.Embed("twitch-embed", {
     width: 1000,
     height: 480,
-    channel: "iparano_tw",
+    channel: "LCSG47",
   });
 </script>
 <br/>
