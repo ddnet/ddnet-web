b50ad2c4a50b7f04cd57cb689142ffa976db1af1
def <dennis@felsin9.de>
2020-08-26 12:43:20 +0200

    Tournament stream info

--- www/index.php
+++ www/index.php
@@ -48,7 +48,7 @@ DDraceNetwork (DDNet) is an actively maintained version of DDRace, a <a href="ht
   new Twitch.Embed("twitch-embed", {
     width: 1000,
     height: 480,
-    channel: "LCSG47",
+    channel: "zikkiee",
   });
 </script>
 {% endcomment %}
