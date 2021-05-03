ebbdd70885dd0e782ca92b35b8b35485e49b311b
Zwelf <zwelf@strct.cc>
2021-05-03 12:52:37 +0200

    Use external menu in libtw2 doc

--- update_libtw2_doc.sh
+++ update_libtw2_doc.sh
@@ -8,9 +8,8 @@ else
 	OUT_DIR="$1"
 fi
 
-INDEX="${OUT_DIR}/index.html"
-
 function file_names() {
+	# When adding/removing files, edit www/_data/menus.yml to update the menu
 	echo connection.md,Connection
 	echo datafile.md,Datafile
 	echo demo.md,Demo
@@ -25,17 +24,6 @@ function file_names() {
 	echo teehistorian.md,Teehistorian
 }
 
-function menu() {
-	echo "menu: |"
-	echo "  <ul>"
-	file_names | while read NAME TITLE
-	do
-		echo "    <li><a href=\"${URL}/${NAME/.md/}\">${TITLE}</a></li>"
-	done
-	echo "  </ul>"
-}
-
-
 function import_article() {
 	local REMOTE_NAME="$1"
 	local TITLE="$2"
@@ -43,7 +31,7 @@ function import_article() {
 	echo "---"
 	echo "title: \"${TITLE}\""
 	echo "layout: default"
-	menu
+	echo "menu-extern: docs-libtw2"
 	echo "---"
 	echo '<div id="global" class="block">'
 	echo ""
@@ -62,42 +50,11 @@ function import_article() {
 		| pandoc --from commonmark --to html \
 
 	echo '</div>'
-
-	# add a link to the index page
-	echo "  <li><a href=\"${REMOTE_NAME/.md/}\">${TITLE}</a></li>" >> $INDEX
-}
-
-function generate_index_pre() {
-	echo "---"
-	echo "title: \"Libtw2 doc\""
-	echo "layout: default"
-	menu
-	echo "---"
-	echo '<div id="global" class="block">'
-	echo ""
-	echo "<!-- This is an auto generated file. If you want to make changes edit scripts/import-twmap2-doc.sh instead -->"
-	echo ""
-	echo "<h2>Libtw2 doc</h2> "
-	echo '<small><i>'
-	echo 'This section is mirrored from the <a href="https://github.com/heinrich5991/libtw2">libtw2</a> documentation and is dual-licensed under MIT or APACHE.</i></small>'
-	echo '</i></small><br>'
-	echo ""
-	echo "Technical documentation of Teeworlds file formats and network protocol"
-	echo ""
-	echo "<ul>"
-}
-
-function generate_index_post() {
-	echo "</ul>"
-	echo "</div>"
 }
 
 mkdir -p "$OUT_DIR"
 
 IFS=','
-
-# regenerate index page
-generate_index_pre > $INDEX
 
 file_names | while read NAME TITLE
 do
@@ -106,6 +63,3 @@ do
 	echo "importing ${NAME} to ${FILENAME}"
 	import_article $NAME $TITLE > ${FILENAME}
 done
-
-generate_index_post >> $INDEX
-
