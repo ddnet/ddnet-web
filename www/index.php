808d0f5a1707f33fca0ebc8ca1814e0f56c6e73f
def <dennis@felsin9.de>
2021-05-19 20:19:13 +0200

    Add donation by c0d3d3v

--- www/index.php
+++ www/index.php
@@ -93,11 +93,6 @@ if ($user_os == 'win32') {
 <a href="discord"><img width="36" src="discord.svg" alt="Discord"/></a>
 <a href="feed/"><img width="36" src="feed.svg" alt="Feed"/></a>
 <a href="https://github.com/ddnet/"><img width="36" src="github.svg" alt="GitHub"/></a>
-
-<div class="right" style="width: 100%; max-width: 25em;">
-  <a href="/funding/"><div class="progressbar" id="funding-total" style="width:100%; margin-top:0.25em"><div class="progress-label"></div></div></a>
-  {% include funding.html %}
-</div>
 </div>
 <br/>
 </div>
