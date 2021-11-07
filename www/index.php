ede7a9a0b1acf061bcd701a241ee2958cfb027b2
def <dennis@felsin9.de>
2021-11-07 22:34:54 +0100

    Version 15.6.2

--- www/index.php
+++ www/index.php
@@ -73,7 +73,7 @@ function getOS() {
 }
 
 $user_os = getOS();
-$version = '15.6.1';
+$version = '15.6.2';
 
 if ($user_os == 'win32') {
   print '<p class="download"><span class="big"><a href="/downloads/DDNet-' . $version . '-win32.zip">Download DDraceNetwork Client &amp; Server ' . $version . ' for Windows (32bit)</a></span><br/><a href="/downloads/">Other systems and versions, changelogs</a></p>';
