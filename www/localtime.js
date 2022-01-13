615bfb627538120d8c6bac2a48c7e702a428d435
Dennis Felsing <dennis@felsin9.de>
2022-01-13 19:29:52 +0100

    2 digit dates

--- www/localtime.js
+++ www/localtime.js
@@ -6,17 +6,27 @@ $(document).ready(function () {
     const hasTitle = typeof $(this).attr('title') !== undefined && $(this).attr('title') !== false;
     if (dateFormat === "time") {
       const text = date.toLocaleTimeString(undefined, {
-          hour: "2-digit",
-          minute: "2-digit",
-        });
+        hour: "2-digit",
+        minute: "2-digit",
+      });
       $(this).text(text);
       if(hasTitle) $(this).attr('title', text);
     } else if (dateFormat === "date") {
-      const text = date.toLocaleDateString();
+      const text = date.toLocaleDateString(undefined, {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+      });
       $(this).text(text);
       if(hasTitle) $(this).attr('title', text);
     } else if (dateFormat === "datetime") {
-      const text = date.toLocaleString();
+      const text = date.toLocaleString(undefined, {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+      });
       $(this).text(text);
       if(hasTitle) $(this).attr('title', text);
     }
