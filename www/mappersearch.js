9f6c9c8acfe5a84a7cd405425f8e0dfa3c2b5bcb
Dennis Felsing <dennis@felsin9.de>
2022-09-22 19:16:46 +0200

    Add mappersearch

---
+++ www/mappersearch.js
@@ -0,0 +1,23 @@
+var mappers = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  remote: '/maps/?mapper=%QUERY',
+  limit: 10
+});
+
+mappers.initialize();
+
+$('#mapperform .typeahead').typeahead(null, {
+  name: 'mappers',
+  displayKey: 'name',
+  source: mappers.ttAdapter(),
+  templates: {
+    suggestion: function(o) {
+      let result = '<p><strong>' + o.mapper.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;") + '</strong>';
+      result += ': ' + o.num_maps + ' maps)</p>'
+      return result;
+    }
+  }
+}).on('typeahead:selected', function(e, data) {
+  $('#mapperform').submit();
+});
