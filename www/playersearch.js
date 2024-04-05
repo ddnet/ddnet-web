7bfd29b44d1041093fa2a5ffd7665479cd291a66
Dennis Felsing <dennis@felsing.org>
2024-04-05 16:53:04 +0200

    Revert "Remove redundant styles, some fixes"

--- www/playersearch.js
+++ www/playersearch.js
@@ -2,12 +2,12 @@ var players = new Bloodhound({
   datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
   queryTokenizer: Bloodhound.tokenizers.whitespace,
   remote: '/players/?query=%QUERY',
-  limit: 12
+  limit: 10
 });
 
 players.initialize();
 
-const typeaheadDataset = {
+$('#playerform .typeahead').typeahead(null, {
   name: 'players',
   displayKey: 'name',
   source: players.ttAdapter(),
@@ -16,12 +16,6 @@ const typeaheadDataset = {
       return '<p><strong>' + o.name.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;") + '</strong><span class="right">' + o.points + '&nbsp;points</span></p>'
     }
   }
-};
-
-$('#playerform .typeahead').typeahead(null, typeaheadDataset).on('typeahead:selected', function(e, data) {
+}).on('typeahead:selected', function(e, data) {
   $('#playerform').submit();
-});
-
-$('#playerform2 .typeahead').typeahead(null, typeaheadDataset).on('typeahead:selected', function(e, data) {
-  $('#playerform2').submit();
 });
