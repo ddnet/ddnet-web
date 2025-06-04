b880c34c2384fbcb2b473a1fefd797eb03853e92
Dennis Felsing <dennis@felsing.org>
2025-06-05 00:02:43 +0200

    Version 19.2.1

--- www/index.php
+++ www/index.php
@@ -76,7 +76,7 @@ function getOS() {
 }
 
 $user_os = getOS();
-$version = '19.2';
+$version = '19.2.1';
 
 if ($user_os == 'win32') {
   print '<p class="download"><span class="big"><a href="/downloads/DDNet-' . $version . '-win32.zip">Download DDraceNetwork Client &amp; Server ' . $version . ' for Windows (32bit)</a></span><br/><a href="/downloads/">Other systems and versions, changelogs</a></p>';
